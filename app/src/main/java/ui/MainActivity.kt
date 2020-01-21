4b2bdd2a80205f85e85beb967b7e33e41efd7205
dailySoftwareProjects <boris.wagner@daily.at>
2020-01-21 17:30:54 +0100

    recyclerView still not working properly

--- app/src/main/java/ui/MainActivity.kt
+++ app/src/main/java/ui/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import androidx.room.Room
 import com.bwappsandmore.doitagain.R
-
 import kotlinx.android.synthetic.main.activity_main.*
-import room.AppDatabase
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,11 +14,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        val appDatabase = Room.databaseBuilder(
-            this,
-            AppDatabase::class.java, "doitagain-list.db"
-        ).build()
 
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
@@ -37,9 +30,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
