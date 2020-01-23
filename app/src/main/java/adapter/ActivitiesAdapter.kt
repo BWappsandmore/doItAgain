bafac68e2382b081c8b7da70ca3b4738112301ab
aspix <10401349+aspix2k@users.noreply.github.com>
2020-01-23 08:58:27 +0300

    Fixed data appearance

--- app/src/main/java/adapter/ActivitiesAdapter.kt
+++ app/src/main/java/adapter/ActivitiesAdapter.kt
@@ -1,38 +1,44 @@
 package adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bwappsandmore.doitagain.R
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.recyclerview_item.view.*
 import room.DoItAgainEntity
 
-class ActivitiesAdapter internal constructor(
-    context: Context
-) : RecyclerView.Adapter<ActivitiesAdapter.ActivitiesViewHolder>() {
-    private val inflater: LayoutInflater = LayoutInflater.from(context)
+class ActivitiesAdapter : RecyclerView.Adapter<ActivitiesViewHolder>() {
+
     private var activities = emptyList<DoItAgainEntity>()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivitiesViewHolder {
-        val itemView = inflater.inflate(R.layout.recyclerview_item, parent, false)
-        return ActivitiesViewHolder(itemView)
-    }
-
-    override fun onBindViewHolder(holder: ActivitiesViewHolder, position: Int) {
-        val current = activities[position]
-        holder.activityItemView.text = current.engagement
-    }
 
     internal fun setActivities(activities: List<DoItAgainEntity>) {
         this.activities = activities
         notifyDataSetChanged()
     }
 
-    override fun getItemCount() = activities.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActivitiesViewHolder =
+        ActivitiesViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.recyclerview_item,
+                parent,
+                false
+            )
+        )
 
-    inner class ActivitiesViewHolder(itemview: View) : RecyclerView.ViewHolder(itemview) {
-        val activityItemView: TextView = itemview.findViewById(R.id.textView)
+    override fun onBindViewHolder(holder: ActivitiesViewHolder, position: Int) {
+        holder.bind(activities[position])
+    }
+
+    override fun getItemCount() = activities.size
+}
+
+class ActivitiesViewHolder(
+    override val containerView: View
+) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+    fun bind(vo: DoItAgainEntity) {
+        itemView.textView.text = vo.engagement
     }
 }
--- app/src/main/java/ui/ShowDBEntriesFragment.kt
+++ app/src/main/java/ui/ShowDBEntriesFragment.kt
@@ -1,49 +1,53 @@
 package ui
 
-
 import adapter.ActivitiesAdapter
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.show_dbentries_fragment.*
 import com.bwappsandmore.doitagain.R
-
+import kotlinx.android.synthetic.main.show_dbentries_fragment.*
+import room.DoItAgainEntity
 
 class ShowDBEntriesFragment : Fragment() {
 
     private lateinit var viewModel: ShowDBEntriesViewModel
-    private lateinit var layout: View
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: ActivitiesAdapter
+    private lateinit var activitiesAdapter: ActivitiesAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        layout = inflater.inflate(R.layout.show_dbentries_fragment, container, false)
-        adapter = ActivitiesAdapter(context!!)
-        recyclerView = layout.findViewById(R.id.recyclerview)
-        recyclerView.addItemDecoration(
-            DividerItemDecoration(
-                context!!,
-                LinearLayoutManager.VERTICAL
-            )
-        )
-        recyclerView.layoutManager = LinearLayoutManager(context!!, LinearLayoutManager.VERTICAL ,false)
-        recyclerView.adapter = adapter
-        return layout
-    }
+    ): View = inflater.inflate(R.layout.show_dbentries_fragment, container, false)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
         viewModel = ViewModelProviders.of(this).get(ShowDBEntriesViewModel::class.java)
+
+        activitiesAdapter = ActivitiesAdapter()
+        recyclerview.apply {
+            addItemDecoration(
+                DividerItemDecoration(
+                    context!!,
+                    LinearLayoutManager.VERTICAL
+                )
+            )
+            layoutManager = LinearLayoutManager(context!!, LinearLayoutManager.VERTICAL, false)
+            adapter = activitiesAdapter
+        }
+
+        // for testing purposes. could be deleted easily
+        activitiesAdapter.setActivities(
+            listOf(
+                DoItAgainEntity(1, "play guitar", 100),
+                DoItAgainEntity(2, "make breakfast", 2),
+                DoItAgainEntity(2, "go out with friends", 20)
+            )
+        )
 
         fab.setOnClickListener {
             val fragmentManager = (activity as MainActivity).supportFragmentManager
