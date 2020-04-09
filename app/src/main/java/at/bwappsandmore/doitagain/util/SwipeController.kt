0b3f3e59cc889528949d384f4c826ae5c83cbfde
dailySoftwareProjects <boris.wagner@daily.at>
2020-04-09 13:25:11 +0200

    buttons drawn on Canvas, swiping on right only possible, because of showing both canvases on left and right.

--- app/src/main/java/at/bwappsandmore/doitagain/util/SwipeController.kt
+++ app/src/main/java/at/bwappsandmore/doitagain/util/SwipeController.kt
@@ -2,16 +2,13 @@ package at.bwappsandmore.doitagain.util
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
+import android.graphics.*
 import android.graphics.drawable.Drawable
 import android.view.MotionEvent
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ItemTouchHelper.*
 import androidx.recyclerview.widget.RecyclerView
-import at.bwappsandmore.doitagain.R
+import at.bwappsandmore.doitagain.R.drawable
 import at.bwappsandmore.doitagain.enums.ButtonState
 import java.lang.Math
 
@@ -33,7 +30,7 @@ class SwipeController(context: Context) : Callback() {
     private val mContext = context
 
     override fun getMovementFlags(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder): Int {
-        return makeMovementFlags(0, LEFT or RIGHT)
+        return makeMovementFlags(0, RIGHT)
     }
 
     override fun onMove(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder, target: RecyclerView.ViewHolder): Boolean {
@@ -77,8 +74,10 @@ class SwipeController(context: Context) : Callback() {
             swipeBack =
                 event.action == MotionEvent.ACTION_CANCEL || event.action == MotionEvent.ACTION_UP
             if (swipeBack) {
-                if (dX < -buttonWidth) buttonShowedState = ButtonState.RIGHT_VISIBLE
-                else if (dX > buttonWidth) buttonShowedState = ButtonState.LEFT_VISIBLE
+                if (dX < -buttonWidth)
+                    buttonShowedState = ButtonState.RIGHT_VISIBLE
+                else if (dX > buttonWidth)
+                    buttonShowedState = ButtonState.LEFT_VISIBLE
 
                 if (buttonShowedState !== ButtonState.GONE) {
                     setTouchDownListener(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
@@ -143,14 +142,16 @@ class SwipeController(context: Context) : Callback() {
         val leftButton = RectF(itemView.left.toFloat(), itemView.top.toFloat(), itemView.left + buttonWidthWithoutPadding, itemView.bottom.toFloat())
         p.color = Color.parseColor("#ff0099cc")
         c.drawRoundRect(leftButton, corners, corners, p)
-        val leftIcon: Drawable? = ContextCompat.getDrawable(mContext, R.drawable.reset_white_24px)
-        leftIcon?.draw(c)
+        val leftIcon: Drawable? = ContextCompat.getDrawable(mContext, drawable.reset_white_24px)
+        val leftIconBitmap = drawableToBitmap(leftIcon!!)
+        c.drawBitmap(leftIconBitmap, leftButton.left + (leftButton.width()/2) - (leftIconBitmap.width.div(2)) , leftButton.top + (leftButton.height()/2)-(leftIconBitmap.height.div(2)),p)
 
         val rightButton = RectF(itemView.right - buttonWidthWithoutPadding, itemView.top.toFloat(), itemView.right.toFloat(), itemView.bottom.toFloat())
         p.color = Color.parseColor("#ff0099cc")
         c.drawRoundRect(rightButton, corners, corners, p)
-        val rightIcon: Drawable? = ContextCompat.getDrawable(mContext, R.drawable.add_alert_white_24px)
-        rightIcon?.draw(c)
+        val rightIcon: Drawable? = ContextCompat.getDrawable(mContext, drawable.add_alert_white_24px)
+        val rightIconBitmap = drawableToBitmap(rightIcon!!)
+        c.drawBitmap(rightIconBitmap,rightButton.left + (rightButton.width()/2) - (rightIconBitmap.width.div(2)), rightButton.top + (rightButton.height()/2)- (rightIconBitmap.height.div(2)),p)
 
         buttonInstance = null
 
@@ -165,5 +166,16 @@ class SwipeController(context: Context) : Callback() {
 
     fun onDraw(c: Canvas) {
         currentItemViewHolder?.let { drawButtons(c, it) }
+    }
+
+    private fun drawableToBitmap(d: Drawable): Bitmap {
+        val bitmap = Bitmap.createBitmap(d.intrinsicWidth, d.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        val c = Canvas(bitmap)
+        d.apply {
+            setBounds(0,0,c.width,c.height)
+            draw(c)
+        }
+
+        return bitmap
     }
 }
